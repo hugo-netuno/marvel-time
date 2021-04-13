f7fc0f93681320efb85bdca3b9d355d255ee02eb
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-13 16:59:25 -0300

    trying to get path

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
-// const path = require('path');
+const path = require('path');
 const appRouter = require('./routes/appRouter');
 
 const app = express();
-// const buildPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '..', 'src');
 const PORT = process.env.PORT || 5000;
 
-// app.use(express.static(buildPath));
+app.use(express.static(buildPath));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
