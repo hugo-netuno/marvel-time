f1cf3a150a363ef0d46d1ec535d0c4b903546dc3
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-13 16:40:37 -0300

    changing app middlewares

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 const appRouter = require('./routes/appRouter');
 
 const app = express();
-const buildPath = path.join(__dirname, '..', 'build');
+// const buildPath = path.join(__dirname, '..', 'build');
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(buildPath));
+// app.use(express.static(buildPath));
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/', appRouter);
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}`));
