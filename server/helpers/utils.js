e660149a368da4f266102aed681a15e87e26119b
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-13 11:01:08 -0300

    created helpers: utils and errors validations

---
+++ server/helpers/utils.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const getter = (data) => {
+  const element = data;
+  return element;
+};
+
+const validateEmail = (email) => {
+  const reg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  return reg.test(email.toString().toLowerCase());
+};
+
+const checkEmail = async (email) => User
+  .findOne({ where: { email } })
+  .then(true)
+  .catch((e) => console.error(e.message));
+
+const generateToken = (email) => jwt.sign(email, 'token');
+
+const verifyToken = (email) => jwt.verify(email, 'token');
+
+module.exports = {
+  checkEmail,
+  getter,
+  validateEmail,
+  generateToken,
+  verifyToken,
+};
---
+++ server/helpers/validationErrors.js
@@ -0,0 +1,94 @@
+const nameLengthError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"displayName" length must be at least 8 characters long';
+  return err;
+};
+const requiredEmailError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"email" is required';
+  return err;
+};
+const validEmailError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"email" must be a valid email';
+  return err;
+};
+const requiredPasswordError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"password" is required';
+  return err;
+};
+const passwordLengthError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"password" length must be 6 characters long';
+  return err;
+};
+const userExistisError = () => {
+  const err = {};
+  err.status = '409';
+  err.message = 'Usuário já existe';
+  return err;
+};
+const emptyEmailError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"email" is not allowed to be empty';
+  return err;
+};
+const emptyPasswordError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = '"password" is not allowed to be empty';
+  return err;
+};
+const invalidDataError = () => {
+  const err = {};
+  err.status = '400';
+  err.message = 'Invalid data';
+  return err;
+};
+const requiredTokenError = () => {
+  const err = {};
+  err.status = '401';
+  err.message = 'Token not found';
+  return err;
+};
+const invalidTokenError = () => {
+  const err = {};
+  err.status = '401';
+  err.message = 'invalid token';
+  return err;
+};
+const userDoesNotExistsError = () => {
+  const err = {};
+  err.status = '404';
+  err.message = 'User not found';
+  return err;
+};
+const userError = () => {
+  const err = {};
+  err.status = '401';
+  err.message = 'Unauthorized user';
+  return err;
+};
+
+module.exports = {
+  nameLengthError,
+  requiredEmailError,
+  validEmailError,
+  requiredPasswordError,
+  passwordLengthError,
+  userExistisError,
+  emptyEmailError,
+  emptyPasswordError,
+  invalidDataError,
+  requiredTokenError,
+  invalidTokenError,
+  userDoesNotExistsError,
+  userError,
+};
