f18651c1dc76cabd4523fe7241876e0b90279a3f
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-13 17:09:18 -0300

    edit package.json scripts

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const appRouter = require('./routes/appRouter');
 
 const app = express();
-const buildPath = path.join(__dirname, '..', 'src');
+const buildPath = path.join(__dirname, '..', 'build');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(buildPath));
