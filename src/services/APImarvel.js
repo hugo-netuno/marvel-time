749894a838eb601f28b8f031554a4249fbfbe1d6
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 10:14:48 -0300

    console logging axios request

--- src/services/APImarvel.js
+++ src/services/APImarvel.js
@@ -6,13 +6,15 @@ function apiLogin(
   displayName,
   password,
   ) {
+    console.log(displayName);
+    console.log(password);
   return axios
     .post(`${API_URL}/login`, {
       displayName,
       password,
     })
     .then((res) => res.data)
-    .catch((err) => console.log(err));
+    .catch((err) => console.log('bad?', err));
 }
 export {
   apiLogin,
