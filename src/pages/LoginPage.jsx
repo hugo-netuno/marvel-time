0e2358ac88f46e7160f95b2aae0871836f65b779
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 08:47:39 -0300

    edited login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,34 @@
+import { apiLogin } from '../services/APImarvel';
+import './pagesStyle.css';
+
 const LoginPage = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = document.querySelector("#displayName").value;
+    const password = document.querySelector("#password").value;
+    await apiLogin(
+      displayName,
+      password,
+    );
+    alert("Login successful");
+  }
   return (
-    <div>
-      <h1 className='login-page-title'>Bem vindo!</h1>
+    <div className="bground">
+      <div className="myStyle">
+        <h1 className='login-page-title'>Bem vindo!</h1>
+        <div className="align">
+          <h2>Login</h2>
+          <form className="loginForm" onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="displayName">Name:
+          <input type="text" name="displayName" id="displayName" />
+            </label>
+            <label htmlFor="password">Password:
+          <input type="text" name="password" id="password" />
+            </label>
+            <input className="submit" type="submit" />
+          </form>
+        </div>
+      </div>
       <footer className='details-footer'>MARVEL APP</footer>
     </div>
   );
