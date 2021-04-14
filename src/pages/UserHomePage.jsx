929385361a5ee78e127b98a727c0a6b38e2470da
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 14:56:05 -0300

    fixed css selector, edited home page

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -7,7 +7,15 @@ const UserHomePage = () => {
         <h1 className='homeTitle'>Home</h1>
         <div className="align">
           <h2>Options</h2>
+          <ul>
+            <li>Edit account settings</li>
+            <li>View Marvel comics</li>
+            <li>View Marvel characters</li>
+          </ul>
         </div>
+      </div>
+      <div>
+        <h1>to do: render component</h1>
       </div>
       <footer className='myStyle footer'>MARVEL APP</footer>
     </div>
