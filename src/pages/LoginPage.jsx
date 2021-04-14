db9727b0c79f546e2403352011080ae5941ed09d
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 08:23:46 -0300

    created login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className='login-page-title'>Bem vindo!</h1>
+      <footer className='details-footer'>MARVEL APP</footer>
+    </div>
+  );
+};
+
+export default LoginPage;
