a9de0ebb7f0d6b42faafc87224b4b5d184115b82
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 23:44:34 -0300

    dotenv fail

--- src/services/APImarvel.js
+++ src/services/APImarvel.js
@@ -1,9 +1,6 @@
 const axios = require('axios');
 const md5 = require('md5');
-const dotenv = require('dotenv');
-
-const result = dotenv.config();
- 
+const dotenv = require('dotenv').config();
 
 const API_URL = 'http://gateway.marvel.com/v1/public';
 const API_PUBLIC_KEY = 'cb183e45d5e03ab355243cad123f6443';
@@ -14,13 +11,12 @@ const displayFunction = (myObj) => {
 
 function apiGetComics() {
   const ts = Date.now().toString();
+  
   console.log('ts', ts);
   console.log('key', process.env.API_PRIVATE_KEY);
   console.log('env', process.env);
-  if (result.error) {
-    throw result.error
-  }
-  console.log('r', result.parsed);
+  console.log('r', dotenv);
+
   return axios
   .post(`${API_URL}/comics`, null, { params: {
       apikey: API_PUBLIC_KEY,
