203d5adec80f0c1ac786863f8570ea17a5a051b3
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 21:45:18 -0300

    added hash to Marvel request

--- src/services/APImarvel.js
+++ src/services/APImarvel.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const md5 = require('md5');
 
 const API_URL = 'http://gateway.marvel.com/v1/public';
 const API_PUBLIC_KEY = 'cb183e45d5e03ab355243cad123f6443';
@@ -8,9 +9,12 @@ const displayFunction = (myObj) => {
 }
 
 function apiGetComics() {
+  const ts = Date.now();
   return axios
     .post(`${API_URL}/comics`, null, { params: {
       apikey: API_PUBLIC_KEY,
+      ts: ts,
+      hash: md5(ts +process.env.API_PRIVATE_KEY+API_PUBLIC_KEY),
       limit: 10,
       callback: displayFunction,
     }})
