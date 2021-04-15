323edd8e65d0094dc4f6237b71bdcd7524a30aef
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-14 21:27:47 -0300

    added callback parameter

--- src/services/APImarvel.js
+++ src/services/APImarvel.js
@@ -3,11 +3,16 @@ const axios = require('axios');
 const API_URL = 'http://gateway.marvel.com/v1/public';
 const API_PUBLIC_KEY = 'cb183e45d5e03ab355243cad123f6443';
 
+const displayFunction = (myObj) => {
+  console.log(myObj);
+}
+
 function apiGetComics() {
   return axios
     .post(`${API_URL}/comics`, null, { params: {
       apikey: API_PUBLIC_KEY,
       limit: 10,
+      callback: displayFunction,
     }})
     .then((res) => res.data)
     .catch((error) => {
